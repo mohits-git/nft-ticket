9aaf2cbbb11c71221096f8ce7d02d47d624daa27
mohits-git <mohitkhatri2805@gmail.com>
2025-01-24 18:20:07 +0530

    [dapp]init: use next js instead

--- client/src/pages/EventListing.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const EventListing = () => {
-  return (
-    <div>EventListing</div>
-  )
-}
-
-export default EventListing
---
+++ dapp/src/app/(landing-page)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h2 className="text-2xl">
+      Hello World
+    </h2>
+  );
+}
